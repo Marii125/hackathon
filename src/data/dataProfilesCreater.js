cabe719eb377b2764a7dad80820b5df3273f718a
Alena Kadecká <kadecka.alena@gmail.com>
2024-06-09 12:06:12 +0200

    created page About Us

--- src/data/dataProfilesCreater.js
+++ src/data/dataProfilesCreater.js
@@ -1,18 +1,19 @@
-export const profilesData = [
-  {
-    id: 'alca',
-    image: '',
-    title: 'Martina Bujasová',
-    description:
-      'Ráda se učím novým věcem a přijímám nové výzvy. Baví mě cestování a objevování nových míst, hlavně restaurací, protože mám ráda dobré jídlo.Moje začátky byly takové, že jsem začala vyhledávat videokurzy, kde jsem se naučila základy HTML, CSS a javaScriptu. Vyhledávala jsem různé stránky, kde jsem se snažila více porozumět fungování kódu. Čim déle jsem vytvářela webové stránky ve volném čase tím více jsem zjišťovala, že to chci v životě dělat. A jsem ráda, že jsem součástí Da Web, protože se můžu více rozvíjet v tomto oboru.',
-    links: [],
-  },
-  {
-    id: 'matka',
-    image: '',
-    title: 'Alena Kadecká',
-    description:
-      'Pochádzam zo Slovenska a žijem v Českej republike už nejaký ten rok. Milujem všetko, čo súvisí s kreativitou, Talianskom, Španielskom, či knihami. Rada sa učím cudzie jazyky, tak sa veľmi teším, že v IT spoznávam aj "iný" druh jazykov ako len tie cudzie. DA Web nie je mojou prvou skúsenosťou s Czechitas. Pred DA Web som si prešla UX Designom a Kóderkou. Aktuálne si neviem predstaviť, čo by ma viac bavilo ako tvorba grafického dizajnu a vývoj webu.',
-    links: [],
-  },
-];
+export const profilesData = {
+  alca: {
+  id: 'alca',
+  image: '',
+  title: 'Martina Bujasová',
+  description:
+    'Ráda se učím novým věcem a přijímám nové výzvy. Baví mě cestování a objevování nových míst, hlavně restaurací, protože mám ráda dobré jídlo.Moje začátky byly takové, že jsem začala vyhledávat videokurzy, kde jsem se naučila základy HTML, CSS a javaScriptu. Vyhledávala jsem různé stránky, kde jsem se snažila více porozumět fungování kódu. Čim déle jsem vytvářela webové stránky ve volném čase tím více jsem zjišťovala, že to chci v životě dělat. A jsem ráda, že jsem součástí Da Web, protože se můžu více rozvíjet v tomto oboru.',
+  links: [],
+},
+matka: {
+  id: 'matka',
+  image: '',
+  title: 'Alena Kadecká',
+  description:
+    'Pochádzam zo Slovenska a žijem v Českej republike už nejaký ten rok. Milujem všetko, čo súvisí s kreativitou, Talianskom, Španielskom, či knihami. Rada sa učím cudzie jazyky, tak sa veľmi teším, že v IT spoznávam aj "iný" druh jazykov ako len tie cudzie. DA Web nie je mojou prvou skúsenosťou s Czechitas. Pred DA Web som si prešla UX Designom a Kóderkou. Aktuálne si neviem predstaviť, čo by ma viac bavilo ako tvorba grafického dizajnu a vývoj webu.',
+  links: [],
+},
+};
+  
