e20d46ae83fdfed40a0c6e6cf64d36a3d98092c2
Martina Bujasová <martina.bujasova2@gmail.com>
2024-06-16 15:11:46 +0200

    Update: add font size and text correction

--- src/components/HistoryIntro/historyintro.jsx
+++ src/components/HistoryIntro/historyintro.jsx
@@ -3,7 +3,7 @@ import './styles.css';
 export const HistoryIntro = () => {
   return (
     <div className="history__info">
-      <h3>Přehled tvých odpovedí:</h3>
+      <h3>Přehled tvých odpovědí:</h3>
     </div>
   );
 };
