bff58db5dc5a3afbd63cc10845cd0c7d56eb3048
Alena Kadecká <kadecka.alena@gmail.com>
2024-06-16 20:06:43 +0200

    almost last update / font siye checked and set up

--- src/components/Result/result.jsx
+++ src/components/Result/result.jsx
@@ -24,9 +24,7 @@ export const Result = ({ score }) => {
   const positionWithMaxPoints = findPositionWithMaxPoints(score);
 
   const positionDetail = Positions[positionWithMaxPoints];
-  // const positionDetail = Positions.find(
-  //   (position) => position.id === positionWithMaxPoints,
-  // );
+ 
 
   return (
     <Profile
